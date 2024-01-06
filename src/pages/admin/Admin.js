db45c1a807b89530861514b5a41f4a389654d492
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2024-01-06 11:57:27 +0500

    Added initial files

---
+++ src/pages/admin/Admin.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Admin.module.scss";
+
+const Admin = () => {
+  return <div>Admin</div>;
+};
+
+export default Admin;
---
+++ src/pages/orderHistory/orderHistory.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./orderHistory.module.scss";
+
+const orderHistory = () => {
+  return <div>orderHistory</div>;
+};
+
+export default orderHistory;
