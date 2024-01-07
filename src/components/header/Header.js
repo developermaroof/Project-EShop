457256246be326a39a3e6593e92ac6fdbf597c1a
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2024-01-07 19:21:06 +0500

    Updated

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,18 +1,51 @@
 import React from "react";
 import "./Header.scss";
 import { Link } from "react-router-dom";
+import { FaShoppingCart } from "react-icons/fa";
+
+const logo = (
+  <div className="logo">
+    <Link to="/">
+      <h2>
+        e<span>Shop</span>.
+      </h2>
+    </Link>
+  </div>
+);
+const cart = (
+  <span className="cart">
+    <Link to="/cart">
+      Cart
+      <FaShoppingCart size={20} />
+      <p>0</p>
+    </Link>
+  </span>
+);
 
 const Header = () => {
   return (
     <header>
       <div className="header">
-        <div className="logo">
-          <Link to="/">
-            <h2>
-              e<span>Shop</span>.
-            </h2>
-          </Link>
-        </div>
+        {logo}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+          <div className="header-right">
+            <span className="links">
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+              <Link to="/order-history">My Orders</Link>
+            </span>
+            {cart}
+          </div>
+        </nav>
+        <div className="menu-icon"></div>
       </div>
     </header>
   );
