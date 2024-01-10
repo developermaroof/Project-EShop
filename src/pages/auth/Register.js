27bc69d067302997b1f4a36ef2f5ea4d43300dbf
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2024-01-10 12:11:20 +0500

    Updated

--- src/pages/auth/Register.js
+++ src/pages/auth/Register.js
@@ -1,35 +1,80 @@
-import React from "react";
+import "react-toastify/dist/ReactToastify.css";
 import "./auth.scss";
+import React, { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 import registerImg from "../../assets/register.png";
 import { Link } from "react-router-dom";
-
 import Card from "../../components/card/Card";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import auth from "../../firebase/config";
 
 const Register = () => {
-  return (
-    <section className="container auth">
-      <Card>
-        <div className="form">
-          <h2>Register</h2>
-          <form>
-            <input type="email" placeholder="Email" required />
-            <input type="password" placeholder="Password" required />
-            <input type="password" placeholder="Confirm Password" required />
-            <button className="--btn --btn-primary --btn-block">
-              Register
-            </button>
-          </form>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [cPassword, setCPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-          <span className="register">
-            <p>Already have an account?</p>
-            <Link to="/login">Login</Link>
-          </span>
+  const registerUser = (e) => {
+    e.preventDefault();
+    if (password !== cPassword) {
+      toast.error("Password do not match!");
+    }
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+  };
+
+  return (
+    <>
+      <ToastContainer />
+      <section className="container auth">
+        <Card>
+          <div className="form">
+            <h2>Register</h2>
+            <form onSubmit={registerUser}>
+              <input
+                type="email"
+                placeholder="Email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                required
+                value={cPassword}
+                onChange={(e) => setCPassword(e.target.value)}
+              />
+              <button type="submit" className="--btn --btn-primary --btn-block">
+                Register
+              </button>
+            </form>
+
+            <span className="register">
+              <p>Already have an account?</p>
+              <Link to="/login">Login</Link>
+            </span>
+          </div>
+        </Card>
+        <div className="img">
+          <img src={registerImg} alt="img" width="400" />
         </div>
-      </Card>
-      <div className="img">
-        <img src={registerImg} alt="img" width="400" />
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
