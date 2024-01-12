6ec354f43e9fbdecaa0e59b2e87556807667e589
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2024-01-12 12:21:07 +0500

    Update Login.js

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,39 +1,86 @@
-import React from "react";
+import { useState } from "react";
 import "./auth.scss";
 import loginImg from "../../assets/login.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FaGoogle } from "react-icons/fa";
 import Card from "../../components/card/Card";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/config";
+import { toast } from "react-toastify";
+import Loader from "../../components/loader/Loader";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const loginUser = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        setIsLoading(false);
+        toast.success("Login Successful...");
+        navigate("/");
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        toast.error(error.message);
+      });
+  };
+
   return (
-    <section className="container auth">
-      <div className="img">
-        <img src={loginImg} alt="img" width="400" />
-      </div>
-      <Card>
-        <div className="form">
-          <h2>Login</h2>
-          <form>
-            <input type="email" placeholder="Email" required />
-            <input type="password" placeholder="Password" required />
-            <button className="--btn --btn-primary --btn-block">Login</button>
-            <div className="links">
-              <Link to="/reset">Reset Password</Link>
-            </div>
-            <p> -- or -- </p>
-          </form>
-          <button className="--btn --btn-danger --btn-block">
-            <FaGoogle color="#fff" />
-            -Login With Google
-          </button>
-          <span className="register">
-            <p>Don't have an account?</p>
-            <Link to="/register">Register</Link>
-          </span>
+    <>
+      {" "}
+      {isLoading && <Loader />}
+      <section className="container auth">
+        <div className="img">
+          <img src={loginImg} alt="img" width="400" />
         </div>
-      </Card>
-    </section>
+        <Card>
+          <div className="form">
+            <h2>Login</h2>
+
+            <form onSubmit={loginUser}>
+              <input
+                type="email"
+                placeholder="Email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button type="submit" className="--btn --btn-primary --btn-block">
+                Login
+              </button>
+              <div className="links">
+                <Link to="/reset">Reset Password</Link>
+              </div>
+              <p> -- or -- </p>
+            </form>
+            <button className="--btn --btn-danger --btn-block">
+              <FaGoogle color="#fff" />
+              -Login With Google
+            </button>
+            <span className="register">
+              <p>Don't have an account?</p>
+              <Link to="/register">Register</Link>
+            </span>
+          </div>
+        </Card>
+      </section>
+    </>
   );
 };
 
